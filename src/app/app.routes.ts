5f76b479873670741feea62c5fa408ddbd3b516c
afshar <afshar.mohebbi@gmail.com>
2024-04-17 19:00:47 +0330

    a simple toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
     { 'path': '', component: AdminComponent },
+    { 'path': 'services', component: ServicesComponent },
 ];
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -3,12 +3,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-admin',
+  selector: 'app-services',
   standalone: true,
   imports: [MatToolbarModule, RouterLink],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.scss'
 })
-export class AdminComponent {
+export class ServicesComponent {
 
 }
