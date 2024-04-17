782de220179b900b2ea1fc504a93080d48a8aaff
afshar <afshar.mohebbi@gmail.com>
2024-04-17 18:16:04 +0330

    admin panel in the menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 'path': '', component: AdminComponent },
+];
