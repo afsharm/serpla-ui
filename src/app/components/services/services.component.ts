c48b34af66afb78857d757a0d8c6415cca148caa
afshar <afshar.mohebbi@gmail.com>
2024-04-18 18:32:43 +0330

    use mat-table to show list of services

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -5,30 +5,40 @@ import { LayoutComponent } from '../layout/layout.component';
 import { ServiceService } from '../../services/service.service';
 import { CommonModule } from '@angular/common';
 import { Service } from '../../models/service.model';
+import {
+  MatTableDataSource, MatTable, MatTableModule, MatCell,
+  MatHeaderCell, MatHeaderRow, MatRow
+} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, LayoutComponent, CommonModule],
+  imports: [MatToolbarModule, RouterLink, LayoutComponent, CommonModule,
+    MatTable, MatCell, MatHeaderCell, MatHeaderRow, MatRow, MatFormField, MatPaginator, MatTableModule],
+  providers: [],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 
 export class ServicesComponent {
 
-  services: Service[] = [];
+  dataSource!: MatTableDataSource<Service>;
+  displayedColumns: string[] = ['id', 'name'];
 
   constructor(private serviceService: ServiceService) { }
 
   ngOnInit(): void {
     this.serviceService.getServices().subscribe(
-      (users: Service[]) => {
-        this.services = users;
+      (services: Service[]) => {
+        this.dataSource = new MatTableDataSource(services);
       },
       (error) => {
         console.error('Error fetching services:', error);
       }
     );
   }
+
 
 }
