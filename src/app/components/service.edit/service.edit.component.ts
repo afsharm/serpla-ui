533465a8e6709040bd4c2db64395172716e2a914
afshar <afshar.mohebbi@gmail.com>
2024-04-21 20:17:31 +0330

    create a form to add data later

--- src/app/components/service.edit/service.edit.component.ts
+++ src/app/components/service.edit/service.edit.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-service-edit',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './service.edit.component.html',
   styleUrl: './service.edit.component.scss'
 })
+
 export class ServiceEditComponent {
+  name = new FormControl('', [Validators.required]);
+
+  errorMessage = '';
+
+  constructor() {
+    merge(this.name.statusChanges, this.name.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.name.hasError('required')) {
+      this.errorMessage = 'You must enter a value';
+    } else {
+      this.errorMessage = '';
+    }
+  }
 
 }
