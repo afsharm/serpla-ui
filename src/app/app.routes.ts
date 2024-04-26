e58f6d1633e291457078d4749e6928b610aa1f24
afshar <afshar.mohebbi@gmail.com>
2024-04-26 13:45:15 +0330

    rename refactoring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
 
 export const routes: Routes = [
-    { 'path': '', component: AdminComponent },
+    { 'path': '', component: HomeComponent },
     { 'path': 'services', component: ServicesComponent },
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,12 +4,12 @@ import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-admin',
+  selector: 'app-home',
   standalone: true,
   imports: [MatToolbarModule, RouterLink, MatIconModule],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
-export class AdminComponent {
+export class HomeComponent {
 
 }
