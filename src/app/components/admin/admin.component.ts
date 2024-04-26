6cffb22d2cbdbc44665180e5d538d8e66e4d5235
afshar <afshar.mohebbi@gmail.com>
2024-04-26 13:27:33 +0330

    added some useful content to the home page

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink],
+  imports: [MatToolbarModule, RouterLink, MatIconModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
